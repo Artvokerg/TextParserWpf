8d41e18d7071651cb400080587ea12dfb965ab0c
art <artyom.grekov@gmail.com>
2025-07-17 18:41:40 +0300

    Добавьте файлы проекта.

---
+++ TextParserWpf/Program .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace TextParserWpf
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<App>();
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+            App app = host.Services.GetService<App>();
+            app?.Run();
+        }
+    }
+}
